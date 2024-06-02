da5f648bc78afeefdd10fba2c2c83724850c0024
Jaymin Darji <jaymindarji2003@gmail.com>
2024-06-02 20:31:03 +0530

    working on curd app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import BodyLayout from "./components/BodyLayout";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <>
+      <BodyLayout />
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
